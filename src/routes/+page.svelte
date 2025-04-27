d7029980abf4776313248f76647b5e3399945e88
Jining Liu <jiningdev@gmail.com>
2025-04-27 14:16:01 -0500

    seo updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <head>
 	<title>DECODE™ Scorer for FTC | 20240 Slingshot</title>
+	<link rel="canonical" href="https://ftcscoring.app/" />
 </head>
 
 <main>
+	<h1>DECODE™ Scorer for the FIRST® Tech Challenge</h1>
+
 	<img id="logo" src="/logo.png" alt="FTC Team 20240 Slingshot Logo" />
 
 	<img
@@ -11,7 +14,7 @@
 		alt="DECODE™ Scorer presented by 20240 Slingshot App Home Screen"
 	/>
 
-	<h1>The ultimate scorer, <b>redefined.</b></h1>
+	<h2>The ultimate scorer, <b>redefined.</b></h2>
 
 	<h3>September 12</h3>
 
@@ -22,8 +25,8 @@
 	>
 
 	<div>
-		<a class="link" href="https://theftc.app" target="_blank">FTC App ↗</a>
-    <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+		<a class="link" href="/ftc-app" target="_blank">FTC App ↗</a>
+		<span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
 		<a class="link" href="https://itd.ftcscoring.app" target="_blank">INTO THE DEEP℠ Scorer ↗</a>
 	</div>
 </main>
@@ -51,12 +54,20 @@
 
 	main {
 		width: 100vw;
-		height: 100vh;
+		min-height: 100vh;
 		padding: 2rem;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+		overflow-y: auto;
+
+		h1 {
+			position: absolute;
+			opacity: 0;
+			top: -100%;
+			left: -100%;
+		}
 
 		#logo {
 			margin: 0 0 calc(min(12rem, 40%) / 16) 0;
@@ -67,7 +78,7 @@
 			width: min(56rem, 100%);
 		}
 
-		h1 {
+		h2 {
 			margin: calc(min(2.8rem, 8vw) / 3) 0 0.2rem 0;
 			font-size: min(2.8rem, 8vw);
 			font-weight: 300;
---
+++ src/routes/ftc-app/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		window.location.href = 'https://theftc.app';
+	});
+</script>
+
+<head>
+	<meta name="robots" content="noindex" />
+</head>
+
+<p>Redirecting to the FTC App...</p>
+<p>presented by 20240 Slingshot</p>
+
+<style>
+	p {
+		font-family:
+			system-ui,
+			-apple-system,
+			BlinkMacSystemFont,
+			'Segoe UI',
+			Roboto,
+			Oxygen,
+			Ubuntu,
+			Cantarell,
+			'Open Sans',
+			'Helvetica Neue',
+			sans-serif;
+	}
+</style>
